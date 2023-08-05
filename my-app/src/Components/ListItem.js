bb14338ab49a9a9560834dc2af5b87dfc5f4bcb3
nitinkaplas <nitin.kaplas1@tcs.com>
2023-08-05 23:43:26 +0530

    add

--- my-app/src/Components/ListItem.js
+++ my-app/src/Components/ListItem.js
@@ -1,26 +1,50 @@
 import AddToCardIcon from "../assests/add_cart.svg"
 import image from "../assests/logo192.png"
-const ListItem=({data})=>{
-    console.log(data)
-    return (
-     <div className={"item-card"}>
-          <img className={"img-fluid"} src={image} alt="{data.title}" /> 
-          <div className={"item-card__information"}>
-               <div>
-                    <span>{data.discountPrice}</span>
-                    <small>
-                         <strike>{data.price}</strike>
-                    </small>
+import { useState } from "react"
+const ListItem = ({ data }) => {
+     console.log(data);
+
+     const [counter, setCounter] = useState(0);
+
+     const increaseCounerByOne = () => {
+          setCounter(counter + 1);
+     }
+
+     const decreaseCounterByOne = () => {
+          if (counter <= 0) return;
+          setCounter(counter - 1);
+     }
+
+     
+     return (
+          <div className={"item-card"}>
+               <img className={"img-fluid"} src={image} alt="{data.title}" />
+               <div className={"item-card__information"}>
+                    <div>
+                         <span>{data.discountedPrice}</span>
+                         <small>
+                              <strike>{data.price}</strike>
+                         </small>
+                    </div>
+                    <div className="title">
+                         <h3>{data.title}</h3>
+                    </div>
                </div>
-               <div className="title">
-                    <h3>{data.title}</h3>
-               </div>
+               {
+                    (counter <= 0) ?
+                         <button className={"cart-add"} onClick={increaseCounerByOne}>
+                              <span>Add To Cart</span>
+                              <img src={AddToCardIcon} alt="Cart Icon" />
+                         </button>
+                         :
+                         <div className="cart-addon">
+                              <button onClick={decreaseCounterByOne}><span>-</span></button>
+                              <span className="{counter}">{counter}</span>
+                              <button onClick={increaseCounerByOne}><span>+</span></button>
+                         </div>
+
+               }
           </div>
-          <button className={"cart-add"}>
-               <span>Add To Cart</span>
-               <img src={AddToCardIcon} alt="Cart Icon"/>
-          </button>
-     </div>
-    )
+     )
 }
 export default ListItem
--- my-app/src/Products/product.js
+++ my-app/src/Products/product.js
@@ -1,36 +1,44 @@
+import { useState } from "react"
 import ListItem from "../Components/ListItem"
-const Item=[
-    {
-        id:0,
-        discountPrice:340,
-        price:450,
-        title:'Title of the Item 1',
-        thumbnail:"logo192.png"
-    },
-    {
-        id:1,
-        discountPrice:150,
-        price:250,
-        title:'Title of the Item 2',
-        thumbnail:"logo192.png"
-    },
-    {
-        id:2,
-        discountPrice:550,
-        price:650,
-        title:'Title of the Item 3',
-        thumbnail:"logo192.png"
-    }
-]
-const Products=()=>{
-     return (
-      <div className="product-list">
-         <div className="product-list--wrapper">
-            <ListItem data={Item[0]}></ListItem>
-            <ListItem data={Item[1]}></ListItem>
-            <ListItem data={Item[2]}></ListItem>
-         </div>
-      </div>
-     )
+
+const Products = () => {
+    const [items,setItem]=useState([
+        {
+            id: 0,
+            discountedPrice: 340,
+            price: 450,
+            title: 'Title of the Item 1',
+            thumbnail: "logo192.png"
+        },
+        {
+            id: 1,
+            discountedPrice: 150,
+            price: 250,
+            title: 'Title of the Item 2',
+            thumbnail: "logo192.png"
+        },
+        {
+            id: 2,
+            discountedPrice: 550,
+            price: 650,
+            title: 'Title of the Item 3',
+            thumbnail: "logo192.png"
+        }
+    ])
+    return (
+        <div className="product-list">
+            <div className="product-list--wrapper">
+                {/* <ListItem data={items[0]}></ListItem>
+                <ListItem data={items[1]}></ListItem>
+                <ListItem data={items[2]}></ListItem> */}
+                {
+                   items.map(item=>{
+                     console.log(item);
+                     return (<ListItem key={item.id} data={item}/>)
+                   })
+                }
+            </div>
+        </div>
+    )
 }
 export default Products
